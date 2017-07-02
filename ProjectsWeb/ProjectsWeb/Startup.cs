ce941d8ffa7651c89e9006cd11f580eeffeb57ce
DESKTOP-SQQ3DD9\M Hamza <iamhamzaqureshi@hotmail.com>
2017-07-03 01:09:51 +0500

    Initial Commit

---
+++ ProjectsWeb/ProjectsWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectsWeb.Startup))]
+namespace ProjectsWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
